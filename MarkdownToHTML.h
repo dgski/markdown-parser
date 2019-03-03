1bf591b660df2ec9ae2f698cca0b368a50fdcaf3
dgski <gorski.dave@gmail.com>
2019-03-02 20:26:48 -0500

    reverse

--- MarkdownToHTML.h
+++ MarkdownToHTML.h
@@ -16,7 +16,7 @@ const regex emptyRegex("");
 // Regular Expressions for in-line elements
 const regex boldRegex("\\*\\*(\\S(.*?\\S)?)\\*\\*");
 const regex italicRegex("\\*(\\S(.*?\\S)?)\\*");
-const regex linkRegex("(?:__|[#])|\\[(.*?)\\]\\((.*?)\\)");
+const regex linkRegex("(?:__|[*#])|\\[(.*?)\\]\\((.*?)\\)");
 const regex imageRegex("!\\[(.+)\\]\\((.+)\\)");
 
 // Represents the current state of the parser - needed for elements spaning multiple lines
